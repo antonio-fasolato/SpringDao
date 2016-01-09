ca1736cdf21bd83c189da90c5c5f7fff4ed3ca1f
fasolato-omicron <fasolato@omicronconsulting.it>
2016-01-09 23:26:04 +0100

    Transactions

--- src/main/java/it/tony/springdao/SpringDaoApplication.java
+++ src/main/java/it/tony/springdao/SpringDaoApplication.java
@@ -3,8 +3,10 @@ package it.tony.springdao;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class SpringDaoApplication {
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(SpringDaoApplication.class, args);
--- src/main/java/it/tony/springdao/controllers/TestController.java
+++ src/main/java/it/tony/springdao/controllers/TestController.java
@@ -3,8 +3,10 @@ package it.tony.springdao.controllers;
 import it.tony.springdao.model.Customer;
 import it.tony.springdao.repositories.ICustomerRepository;
 import it.tony.springdao.services.CustomerService;
+import it.tony.springdao.services.ServiceException;
 import it.tony.springdao.tables.CustomerTable;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,29 +19,31 @@ public class TestController {
     private CustomerService customerService;
 
     @RequestMapping("/")
-    public String test() {
+    @Transactional(rollbackFor = {ServiceException.class})
+    public String test() throws ServiceException {
         try {
-//            customerService.save(new Customer("Antonio", 35));
+            customerService.save(new Customer("Antonio", 35));
 //            Iterable<CustomerTable> all = customerRepository.findAll();
 //            for (CustomerTable t : all) {
 //                System.out.println(t.getEntity());
 //            }
 
-//            CustomerTable t = customerRepository.findOne(7);
-//            System.out.println(t.getEntity());
-//            t.getEntity().setName("Modified");
-//            customerService.save(t.getEntity());
+            CustomerTable t = customerRepository.findOne(7);
+            System.out.println(t.getEntity());
+            t.getEntity().setName("Modified");
+            customerService.save(t.getEntity());
 
             Iterable<Customer> l = customerService.getAllOrderByInsertDate();
-//            Customer last = null;
+            Customer last = null;
             for(Customer c : l) {
-//                last = c;
-//                System.out.println(c.getId());
-                System.out.println(c);
+                last = c;
+                System.out.println(c.getId());
+//                System.out.println(c);
             }
-//            customerService.delete(last);
+            customerService.delete(last);
         } catch (Exception e) {
             e.printStackTrace();
+            throw new ServiceException(e);
         }
         return "Test";
     }
